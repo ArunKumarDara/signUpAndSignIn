bb8ec157b1414674dbb9f0631c2581fcd4c093f1
Arun Kumar <mymailarun32@gmail.com>
2024-02-02 13:03:20 +0530

    feat(routes/users): implement JWT token and cookie parser for user authentication

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,5 +1,6 @@
 import express from "express";
-import User from "../models/user";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
 
 const router = express.Router();
 
@@ -13,8 +14,21 @@ router.post("/register", async (req, res) => {
     }
     user = new User(req.body);
     await user.save();
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("auth token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+    return res.sendStatus(200);
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Something went wrong" });
   }
 });
+
+export default router;
