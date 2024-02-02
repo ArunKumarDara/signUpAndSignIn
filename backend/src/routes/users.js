9657f619bc96f003314cb8fa0c7e24ccef07b59b
Arun Kumar <mymailarun32@gmail.com>
2024-02-02 11:35:26 +0530

    feat(routes/user):implement basic routes for user creation

---
+++ backend/src/routes/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+import User from "../models/user";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+    user = new User(req.body);
+    await user.save();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Something went wrong" });
+  }
+});
