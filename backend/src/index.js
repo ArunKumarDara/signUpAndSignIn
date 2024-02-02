509ea2615df9e996d59beaaf6650322de4e17d30
Arun Kumar <mymailarun32@gmail.com>
2024-02-02 11:37:02 +0530

    feat(index):implement MongoDB backend connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,25 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
 
+const url = process.env.MONGODB_CONNECTION_STRING;
+
+mongoose.connect(url);
+
 const app = express();
+app.use(express.json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({ message: "Hello World!" });
 });
 
 app.listen(process.env.PORT || 3000, () => {
